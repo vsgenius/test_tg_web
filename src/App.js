7473d40a9c0e24ec7fdd157d68dc876ef5fff100
Vladimir Belich <belichvladimir@mail.ru>
2022-12-11 13:51:16 +0300

    5 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 
-
+const tg = window.Telegram.WebApp
 
 function App() {
+  useEffect(()=>{
+    tg.ready()
+  },[])
+  const onClose = () => {
+    tg.close()
+  }
   return (
     <div className="App">
       work
-    <button>Закрыть</button>
+    <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
