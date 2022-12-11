2634b1b4008014fd932da4a6eba057084a9e0cd2
Vladimir Belich <belichvladimir@mail.ru>
2022-12-11 12:51:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import './App.css';
+
+
+ const tg = window.Telegram.Webapp;
+
+function App() {
+  // useEffect(()=>{
+  //   tg.ready();
+  // },[])
+  // const onClose = () => {
+  //   tg.close()
+  // }
+  return (
+    <div className="App">
+      work
+    <button >Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
